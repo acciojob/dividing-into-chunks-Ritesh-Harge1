a57cd493816b3eb813cc360d6599cbbe69456efa
RITESH HARGE <135429163+Ritesh-Harge1@users.noreply.github.com>
2025-08-26 10:51:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
+function divide(arr, n) {
+  let result = [];
+  let currentChunk = [];
+  let currentSum = 0;
 
-const divide = (arr, n) => {
-  // Write your code here
-};
+  for (let num of arr) {
+    if (currentSum + num <= n) {
+      currentChunk.push(num);
+      currentSum += num;
+    } else {
+      // push current chunk to result
+      if (currentChunk.length > 0) {
+        result.push(currentChunk);
+      }
+      // start new chunk
+      currentChunk = [num];
+      currentSum = num;
+    }
+  }
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+  // push last chunk if exists
+  if (currentChunk.length > 0) {
+    result.push(currentChunk);
+  }
+
+  return result;
+}
+
+// Example usage
+console.log(divide([1, 2, 3, 4, 1, 0, 2, 2], 5)); 
+// [[1, 2], [3], [4, 1, 0], [2, 2]]
+
+console.log(divide([4, 3, 2, 1], 4)); 
+// [[4], [3], [2, 1]]
+
+console.log(divide([2, -1, 3, 2], 4)); 
+// [[2, -1, 3], [2]]
+
